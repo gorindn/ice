a53547a254234fd6d2e17fe050d9c8afb8a9742b
Jordan Deyton <deytonjh@ornl.gov>
2015-04-22 15:19:25 -0400

    Started on a PythonRemoteCore for ICE.
    In keeping with the naming scheme used for native libraries, I have
    created a new PyDev (Python) project called
    "org.eclipse.ice.core.python" under the directory src/python.
    
    It has a src folder with the package structure org/eclipse/ice/core
    (which mirrors the Java package structure).
    
    Signed-off-by: Jordan Deyton <deytonjh@ornl.gov>

---
+++ src/python/org.eclipse.ice.core.python/src/org/eclipse/ice/core/Core.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr 22, 2015
+
+@author: Jordan H. Deyton
+'''
+
+class Core(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
